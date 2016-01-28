c5f4b2a243cbe979428973032a281e111bdc4d60
Jack Hu <hutufeia2009@sina.com>
2016-01-28 17:07:32 +0800

    fix seed.js

--- server/config/seed.js
+++ server/config/seed.js
@@ -4,49 +4,49 @@
 
 'use strict';
 
-var mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Article = mongoose.model('Article'),
-	TagCategory = mongoose.model('TagCategory'),
-	Tag = mongoose.model('Tag');
+const mongoose = require('mongoose');
+const	User = mongoose.model('User');
+const	Article = mongoose.model('Article');
+const	TagCategory = mongoose.model('TagCategory');
+const	Tag = mongoose.model('Tag');
 
-	//初始化标签,文章,用户
-	function initData() {
-		return function *(next) {
-			if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'){
-				const userCount = yield User.count().exec();
-				if(userCount === 0){
-					yield User.remove().exec();
-					yield User.create({
-					 			nickname:'admin',
-					 			email:'admin@admin.com',
-					 			role:'admin',
-					 			password:'admin',
-					 			status:1
-					 		},{
-					 			nickname:'test001',
-					 			email:'test001@test.com',
-					 			role:'user',
-					 			password:'test',
-					 			status:1
-					 		},{
-					 			nickname:'test002',
-					 			email:'test002@test.com',
-					 			role:'user',
-					 			password:'test',
-					 			status:2
-					 		},{
-					 			nickname:'test003',
-					 			email:'test003@test.com',
-					 			role:'user',
-					 			password:'test',
-					 			status:0
-					 		}).exec();
-				}
-				const tagCount = yield TagCategory.count().exec();
+//初始化标签,文章,用户
+function initData() {
+	return function *(next) {
+		if(process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'){
+			const userCount = yield User.count();
+			if(userCount === 0){
+				yield User.remove();
+				yield User.create({
+		 			nickname:'admin',
+		 			email:'admin@admin.com',
+		 			role:'admin',
+		 			password:'admin',
+		 			status:1
+		 		},{
+		 			nickname:'test001',
+		 			email:'test001@test.com',
+		 			role:'user',
+		 			password:'test',
+		 			status:1
+		 		},{
+		 			nickname:'test002',
+		 			email:'test002@test.com',
+		 			role:'user',
+		 			password:'test',
+		 			status:2
+		 		},{
+		 			nickname:'test003',
+		 			email:'test003@test.com',
+		 			role:'user',
+		 			password:'test',
+		 			status:0
+		 		});
+			}
+			const tagCount = yield TagCategory.count();
 				if(tagCount === 0){
-					yield TagCategory.remove().exec();
-					yield Tag.remove().exec();
+					yield TagCategory.remove();
+					yield Tag.remove();
 					const languageCat = yield TagCategory.create({
 								name:'language',
 								desc:'按编程语言分类'
@@ -63,11 +63,11 @@ var mongoose = require('mongoose'),
 									name:'react',
 									cid:languageCat._id,
 									is_show:true
-								}).exec();
+								});
 					const systemCat = yield TagCategory.create({
-								name:'system',
-								desc:'按操作系统分类'
-							}).exec();
+																		name:'system',
+																		desc:'按操作系统分类'
+																	});
 					yield Tag.create({
 									name:'linux',
 									cid:systemCat._id,
@@ -80,20 +80,20 @@ var mongoose = require('mongoose'),
 									name:'android',
 									cid:systemCat._id,
 									is_show:true
-								}).exec();
+								});
 					const otherCat = yield TagCategory.create({
-								name:'other',
-								desc:'其它分类'
-							}).exec();
+																	name:'other',
+																	desc:'其它分类'
+																});
 					yield Tag.create({
 									name:'git',
 									cid:otherCat._id,
 									is_show:true
-								}).exec();
+								});
 
-					const tags = yield Tag.find().exec();
-					yield Article.remove().exec();
-					tags.map(function (tag,index) {
+					const tags = yield Tag.find({});
+					yield Article.remove();
+					yield tags.map(function (tag,index) {
 						var indexOne = parseInt(index) +1;
 						var indexTwo = parseInt(index) +2;
 						Article.create({
@@ -106,7 +106,7 @@ var mongoose = require('mongoose'),
 							content:'<p>我第' + (index + indexTwo) + '次爱你.</p>',
 							tags:[tag._id],
 							status:1
-						}).exec();
+						});
 					});
 				}
 			}
